0ee3ab18f8da5d06cfcb97f64ec48e13503329af
Rachit Kumar <eric_the_red@Rachits-MacBook-Pro.local>
2020-02-26 00:02:36 +0530

    collective done

--- collective.cpp
+++ collective.cpp
@@ -55,24 +55,33 @@ int main(int argc, char **argv)
 {
 
 	int processes,rank;
+
+	int vars[2];
+
+	float *A;//n*m
+	float *B;//m*n
+	float *C;//n*n
+	float *C_serial;//n*n
 	int n;
 	int m;
-	// cin>>n;
-	n=10000;
+	n=stoi(argv[1]);
 	m=32;
-	float *A=new float[n*m];//n*m
-	float *B=new float[m*n];//m*n
-	float *C=new float[n*n];//n*n
-	float *C_serial=new float[n*n];//n*n
+
+	A=new float[n*m];//n*m
+	B=new float[m*n];//m*n
+	C=new float[n*n];//n*n
+	C_serial=new float[n*n];//n*n
 
 	MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int i;
+
     cout<<"rank:"<<rank<<" processes:"<<processes<<endl;
     std::chrono::time_point<std::chrono::system_clock> begin = std::chrono::system_clock::now();
 	if(rank==0)
 	{
+		vars[0]=n;
+		vars[1]=m;
 		for (int i = 0; i < n; i++)
 		{  
 			for (int j = 0; j < m; j++)
@@ -81,47 +90,46 @@ int main(int argc, char **argv)
 				B[j+i*m] = float (rand()%10)+0.1;
 			}
 		}
-
-
-	}
-	MPI_Bcast(A, n*m , MPI_FLOAT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(B, m*n , MPI_FLOAT, 0, MPI_COMM_WORLD);		
-
-	int startrow = rank * ( n / processes);
-	int endrow = ((rank + 1) * ( n / processes));
-
-	float *C_temp=new float[n*n/processes];// n*n/processes
 	
+	}
+
+	MPI_Bcast(&vars, 2, MPI_INT, 0, MPI_COMM_WORLD);	\
+	MPI_Bcast(A, vars[0]*vars[1] , MPI_FLOAT, 0, MPI_COMM_WORLD);	\
+	MPI_Bcast(B, vars[1]*vars[0] , MPI_FLOAT, 0, MPI_COMM_WORLD);	
+
+	int startrow = rank * ( vars[0] / processes);
+	int endrow = ((rank + 1) * ( vars[0] / processes));
+
+	float *C_temp=new float[vars[0]*vars[0]/processes];// n*n/processes
+
 	int l=0;
     for (int i = startrow; i < endrow; i++) 
     {
-        for (int j = 0; j < n; j++) 
+        for (int j = 0; j < vars[0]; j++) 
         {
         	C_temp[l]=0;
-            for (int k = 0; k < m; k++) 
+            for (int k = 0; k < vars[1]; k++) 
             {
-                C_temp[l] += A[ (i*m + k) ] * B[ (k*n + j) ];
+                C_temp[l] += A[ (i*vars[1] + k) ] * B[ (k*vars[0] + j) ];
             }
             l++;
         }
     }
 
-    MPI_Gather(C_temp, n*n/processes, MPI_FLOAT, C, n*n/processes,  MPI_FLOAT, 0, MPI_COMM_WORLD);
-    cout<< (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - begin)).count()<<endl;
+    MPI_Gather(C_temp, vars[0]*vars[0]/processes, MPI_FLOAT, C, vars[0]*vars[0]/processes,  MPI_FLOAT, 0, MPI_COMM_WORLD);
+    cout<<"parallel time: "<< (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - begin)).count()<<endl;
 
     if(rank==0)
     {
     	std::chrono::time_point<std::chrono::system_clock> begin = std::chrono::system_clock::now();
-    	Multiply_serial(A,B,C_serial,n,m,n); 
-    	cout<< (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - begin)).count()<<endl;
+    	Multiply_serial(A,B,C_serial,vars[0],vars[1],vars[0]); 
+    	cout<<"serial time: "<< (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - begin)).count()<<endl;
 
-    	cout<<IsEqual(C,C_serial,n,n)<<endl;
-    	//cout<< (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - begin)).count()<<endl;
-
+    	cout<<"IsEqual: "<<IsEqual(C,C_serial,vars[0],vars[0])<<endl;
+    	// cout<< (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - begin)).count()<<endl;
     }
 
 	// MPI_Barrier(MPI_COMM_WORLD);
-
 
 	MPI_Finalize();
 	return 0;
